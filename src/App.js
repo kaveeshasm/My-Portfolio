11bb2d7db87a8627049bba9585d1e62500d99e35
kaveeshasm <kaveeshasandeepanim@gmail.com>
2025-09-09 20:07:29 +0530

    Files Updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from './Components/NavBar/NavBar';
 import Home from './Components/Home/Home';
